6e89aba68d66d0a52330a08f5327268f31f947f1
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2022-08-27 20:30:51 +0530

    fixed promise get connection

--- config/db.js
+++ config/db.js
@@ -1,19 +1,21 @@
 const mysql = require("mysql2/promise");
 
 const mydb = mysql.createPool({
-  connectionLimit : 5, //important
+  connectionLimit: 5, //important
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  debug    :  false
+  debug: false,
 });
 
-mydb.getConnection((err,connection)=> {
-  if(err)
-  throw err;
-  console.log("Successfully connected to the database.");
-  connection.release();
+mydb.getConnection()
+  .then((conn) => {
+    console.log("Successfully connected to the database.");
+    conn.release();
+  })
+  .catch((e) => {
+    console.log(e);
 });
 
 
